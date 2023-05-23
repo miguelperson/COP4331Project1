2fc81f628e095c63710a71be00daca796d80712d
Ryan Kolano <69911566+ll-KOYA-ll@users.noreply.github.com>
2023-05-22 21:12:06 -0400

    -uploaded supporting files for contacts.html -uploaded contacts.html

---
+++ js/contacts.js
@@ -0,0 +1,17 @@
+// testing function to see if something works
+function doSomething()
+{
+    alert("Hello WWW!");
+}
+
+// adding a contact functions
+
+
+// editing a contact functions
+
+
+// removing a contact functions
+
+
+// logging off functions
+
