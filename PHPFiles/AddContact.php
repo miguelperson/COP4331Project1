d62dc2640fead253746cb1321cac4527b86505a7
John Doe <john@doe.org>
2023-05-24 17:08:29 -0400

    Started add contack api

--- PHPFiles/AddContact.php
+++ PHPFiles/AddContact.php
@@ -1,3 +1,21 @@
-<$php
+<?php
+
 	$userInfo = getRequestInfo();
+	$id;
+	$login;
+	$password;
+	$firstName;
+	$lastName;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else 
+	{
+
+	}
+
+
 ?>
