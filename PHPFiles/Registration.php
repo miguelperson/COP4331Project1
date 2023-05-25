e96c778f8fc4e5e321159a351bf77189f2f663da
Miguel Baca-Urteaga <miguelbaca@Knights.ucf.edu>
2023-05-25 12:33:29 -0400

    Creating connection and attributes for registration file

--- PHPFiles/Registration.php
+++ PHPFiles/Registration.php
@@ -1,1 +1,14 @@
+<?php
+$inData = getRequestInfo();
 
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+	$email = "";
+	$phoneNumber = "";
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+
+
+?>
