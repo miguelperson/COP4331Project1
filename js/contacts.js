d9607b1ff549a4f65aba20dee0655541cd314f15
Ryan Kolano <69911566+ll-KOYA-ll@users.noreply.github.com>
2023-05-24 22:25:11 -0400

    Added basic functionality to adding a contact
    -Allows the user to open/close the "add contact" form when adding a user.

--- js/contacts.js
+++ js/contacts.js
@@ -1,17 +1,36 @@
-// testing function to see if something works
-function doSomething()
-{
-    alert("Hello WWW!");
-}
+const addContactButton = document.getElementById("addContactButton");
+const closeAddContactButton = document.getElementById("addContactCloseButton");
+
+const searchButton = document.getElementById('searchButton');
+
+// searching a contact functions
+
 
 // adding a contact functions
+addContactButton.addEventListener("click", function() {
+    document.querySelector(".addContactForm").style.display = "block";
+})
 
+closeAddContactButton.addEventListener("click", function() {
+    document.querySelector(".addContactForm").style.display = "none";
+
+})
 
 // editing a contact functions
-
+function editContact()
+{
+    alert("edit");
+}
 
 // removing a contact functions
+function removeContact()
+{
+    alert("remove");
+}
 
-
-// logging off functions
+// logging out functions
+function logout()
+{
+    alert("logout");
+}
 
