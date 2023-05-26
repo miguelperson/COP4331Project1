7ea16f70330435e8d749726a8b871ceda2379369
Miguel Baca-Urteaga <miguelbaca@Knights.ucf.edu>
2023-05-25 22:51:14 -0400

    First attempt at creating the registration php file

--- LAMPAPI/Registration.php
+++ LAMPAPI/Registration.php
@@ -1,19 +1,42 @@
 <?php
-	$userInfo = getRequestInfo();
 
-	$id = 0;
-	$firstName = $userInfo["firstname"];
-	$lastName = $userInfo["lastname"];
-	$username = $userInfo["username"];
-	$password = $userInfo["password"];
+$requestData = getRequestData();
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}else{
-		$statement = $conn->prepare("INSERT INTO,firstName,lastName");
-	}
+$firstName = $requestData["firstName"];
+$lastName = $requestData["lastName"];
+$login = $requestData["login"];
+$password = $requestData["password"];
 
+$databaseConnection = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if ($databaseConnection->connect_error) {
+    returnWithError($databaseConnection->connect_error);
+} else {
+    $sql = "SELECT * FROM Users WHERE Login=?";
+    $statement = $databaseConnection->prepare($sql);
+    $statement->bind_param("s", $login);
+    $statement->execute();
+    $result = $statement->get_result();
+    $rowCount = $result->num_rows;
+    if ($rowCount == 0) {
+        $insertStatement = $databaseConnection->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?,?,?,?)");
+        $insertStatement->bind_param("ssss", $firstName, $lastName, $login, $password);
+        $insertStatement->execute();
+        $insertId = $insertStatement->insert_id;
+        $insertStatement->close();
+        $databaseConnection->close();
+        http_response_code(200);
+        $responseData = '{"id": "' . $insertId . '"}';
+        header('Content-type: application/json');
+        echo $responseData;
+    } else {
+        http_response_code(409);
+        $errorData = '{"error":"Username taken"}';
+        header('Content-type: application/json');
+        echo $errorData;
+    }
+}
 
-?>
+function getRequestData()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
