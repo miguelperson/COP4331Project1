9589c67f66cecb9a290621ad5cf3375f05952525
Ryan Kolano <69911566+ll-KOYA-ll@users.noreply.github.com>
2023-05-30 19:47:20 -0400

    removed dataCreated table entry

--- js/contacts.js
+++ js/contacts.js
@@ -23,12 +23,10 @@ const logoutButton = document.getElementById('logoutButton');
 // selected table row for edit/delete
 var selectedRow = null;
 
-//array of contact id objects {"id" : 34}
+// array of contact id objects {"id" : 34}
 var contactID = [];
 
-
-
-//intial load contacts
+// initial load contacts
 loadContacts();
 
 // searching a contact functions -------------------------------------------------------------------------------------------------
@@ -38,11 +36,9 @@ function searchContact() {
     let searchInput1 = document.getElementById("search1").value;
     let searchInput2 = document.getElementById("search2").value;
 
+    // send as string to api
     if (searchInput2 == "") {
-        tableRows.forEach((row,i)=> {
-            let tableData = row.textContent;
-            row.classList.toggle('hide');
-        })
+
     } else {
         
     }
@@ -61,6 +57,7 @@ addContactFormButton.addEventListener("click", function() {
         // proceed with adding the contact record
         var formData = readAddContactFormData();
         if (selectedRow == null) {
+            insertNewRecord(formData);
             loadContacts();
         } else {
             updateRecord(formData);
@@ -102,7 +99,6 @@ function readAddContactFormData() {
     formData["lastName"] = document.getElementById("lastName").value;
     formData["phone"] = document.getElementById("phone").value;
     formData["email"] = document.getElementById("email").value;
-    formData["dateCreated"] = new Date().toLocaleString().split(',')[0];
     return formData;
 }
 
@@ -186,9 +182,7 @@ function insertNewRecord(data) {
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = data.email;
     cell5 = newRow.insertCell(4);
-    cell5.innerHTML = data.dateCreated;
-    cell6 = newRow.insertCell(5);
-    cell6.innerHTML = `<a onClick="edit(this)">Edit</a> <a onClick="removeContact(this)">Remove</a>`;
+    cell5.innerHTML = `<a onClick="edit(this)">Edit</a> <a onClick="removeContact(this)">Remove</a>`;
 }
 
 function resetAddContactForm() {
@@ -221,6 +215,7 @@ editContactFormButton.addEventListener("click", function() {
     if (editValidate()) {
         var formData = readEditContactFormData();
         if (selectedRow == null) {
+            insertNewRecord(formData);
             loadContacts();
         }
         else {
@@ -242,7 +237,6 @@ function readEditContactFormData() {
     formData["lastName"] = document.getElementById("lastName2").value;
     formData["phone"] = document.getElementById("phone2").value;
     formData["email"] = document.getElementById("email2").value;
-    formData["dateCreated"] = new Date().toLocaleString().split(',')[0];
     return formData;
 }
 
@@ -356,7 +350,6 @@ function removeContact(td) {
     removeContactRecord.name = selectedRow.cells[0].innerHTML + " " + selectedRow.cells[1].innerHTML;
     removeContactRecord.phoneNumber = selectedRow.cells[2].innerHTML;
     removeContactRecord.email = selectedRow.cells[3].innerHTML;
-    removeContactRecord.dateCreated = selectedRow.cells[4].innerHTML;
     removeContactRecord.id = sessionStorage.getItem("id");
 
     console.log(removeContactRecord);
@@ -411,27 +404,22 @@ logoutButton.addEventListener("click", function() {
 
 function loadContacts(){
     let user = {};
-
     user.id = sessionStorage.getItem("id");
-
     fetch("LAMPAPI/LoadContacts.php", {
         "method": "POST",
         "headers" :{
             "Content-Type" : "application/json; charset=utf-8" 
         },
-
         "body": JSON.stringify(user)
-
     }).then(function(response){
-
         return response.text();
-
     }).then(function(data){
         console.log(data);
         let info = JSON.parse(data);
         let newData = nameSplit(info);
         for(let i= 0 ; i < info.results.length; i++){
             console.log(newData[i]);
+
 
             //stores contact id based on row number
             contactID[i] = {
@@ -442,16 +430,12 @@ function loadContacts(){
             //let item = "" + i;
             //sessionStorage.setItem(item, info.results[i].ID);
         }
-
         //sessionStorage.setItem("length", info.results.length);
-
     });
 }
-
 //function to split name into first and last and return the dataset
 function nameSplit(info){
     let retval = []; 
-
     for(let i = 0; i < info.results.length; i++){
         let arr = info.results[i].name.split(" ");
         retval[i] = {
