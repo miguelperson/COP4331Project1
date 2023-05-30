e3d4d44fcb6de5a4e15ccc14d8b56f602fb8359f
miguelperson <miguelbaca@knights.ucf.edu>
2023-05-29 20:55:35 -0400

    Added the needed attributes to update the contact info

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,3 +1,12 @@
 <?php
 $inData = json_decode(file_get_contents('php://input'), true);
+$contactID = $inData["contactID"];
+    $name = $contactInfo["name"];
+	$email = $contactInfo["email"];
+	$phoneNumber = $contactInfo["phone"];
+
+// create connection
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 
+
+
 ?>
