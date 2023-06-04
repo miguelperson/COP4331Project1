1891086e7057f185bfe008cd568969a1b4cd786f
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-06-04 16:19:06 -0400

    debugging SearchContacts

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -1,4 +1,8 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
     $inData = json_decode(file_get_contents('php://input'), true);
     $userID = $inData["userID"];
     $searchQuery = $inData["query"];
