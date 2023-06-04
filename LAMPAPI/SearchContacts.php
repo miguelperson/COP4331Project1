61368bf447a95d235e962d4a90e8f6aee5337d3e
||-KOYA-|| <ryanskolano@gmail.com>
2023-06-04 01:56:43 -0400

    -testing serach function

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -5,6 +5,8 @@
     $searchResults = "";
     $searchCount = 0;
 
+	$echo search;
+
     // create connection
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
 	// check connection 
@@ -14,13 +16,14 @@
 	}
     else 
     {
+		$echo "1";
 		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE Name like? AND UserID=?");
 		$colorName = "%" . $inData["search"] . "%";
 		$stmt->bind_param("sss", $colorName, $colorName, $inData["userId"]);
 		$stmt->execute();
 
         $result = $stmt->get_result();
-
+		$echo "2";
         while($row = $result->fetch_assoc())
 		{
 			if( $searchCount > 0 )
@@ -30,7 +33,7 @@
 			$searchCount++;
 			$searchResults .= '{"FirstName" : "' . $row["FirstName"]. '", "LastName" : "' . $row["LastName"]. '", "PhoneNumber" : "' . $row["PhoneNumber"]. '", "EmailAddress" : "' . $row["EmailAddress"]. '", "UserID" : "' . $row["UserID"].'", "ID" : "' . $row["ID"]. '"}';
 		}
-
+		$echo "3";
         if( $searchCount > 0 )
 		{
             returnWithInfo( $searchResults );
@@ -39,13 +42,14 @@
 		{
 			returnWithError( "No Records Found" );
 		}
-
+		$echo "4";
         // Return the rows as JSON
         // Return the rows as JSON
         echo json_encode($rows);
 
         $stmt->close();
 		$conn->close();
+		$echo "5";
     }
 
     function returnWithInfo( $searchResults )
